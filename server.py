6a8cc30ad8e7ca6185cae6394d288c8f390101e5
anandmakadiya4387 <anandmakadiya4387@gmail.com>
2026-09-10 20:37:32 +0530

    Update server.py

--- server.py
+++ server.py
@@ -373,7 +373,7 @@ def create_fastapi_app():
     app = FastAPI(title="Anand Clinic Sync API", version="53")
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origin_regex=r".*",
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
